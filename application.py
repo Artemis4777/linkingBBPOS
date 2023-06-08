5c441b505878dff86b28181d7a4c0335dcb9662c
Artemis4777 <cberger0354@gmail.com>
2023-06-08 15:28:14 -0400

    first try

---
+++ application.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, redirect
+import urllib.parse
+
+# declare app
+application = Flask(__name__)
+
+
+# serve main page
+@application.route("/")
+def index():
+    requestParametes = request.args.to_dict()
+    set(k.lower() for k in requestParametes)
+    print(requestParametes)
+    if "xredirecturl" not in requestParametes:
+        requestParametes["xredirecturl"] = "https://www.cardknoxdeeplinktest.com/results"
+    if "xkey" not in requestParametes:
+        requestParametes["xkey"] = "artemisdev12345"
+    newLink = "dck://app.cardknox.com/transaction?" + urllib.parse.urlencode(requestParametes, doseq=True)
+    print(newLink)
+    return redirect(newLink, code=302)
+
+# parameters to run with
+if __name__ == "__main__":
+    application.run(debug=False)
